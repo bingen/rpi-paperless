6ad3d40c8fd3e914d4d7df233e4c01e3839205a5
ßingen <bingentxu@gmail.com>
2018-02-13 00:14:30 +0100

    tmp: ftp server doesn't work with Docker swarm
    I think the problem is the IP address translation docker swarm does, as I see with `tcpdump`:
    ```
    19:34:53.044711 IP 10.255.0.3.46096 > 10.255.0.4.21: Flags [P.], seq 36:63, ack 97, win 229, options [nop,nop,TS val 3290369629 ecr 129178698], length 27: FTP: PORT 192,168,1,137,152,71
    19:34:53.045589 IP 10.255.0.4.21 > 10.255.0.3.46096: Flags [P.], seq 97:124, ack 63, win 219, options [nop,nop,TS val 129179616 ecr 3290369629], length 27: FTP: 500 Illegal PORT command.
    ```
    As you can see the packet origin address (`10.255.0.3`) differs from the real origin one (`192.168.1.137`) which is advertised by FTP.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,6 +11,10 @@ usermod -d ${PAPERLESS_CONSUMPTION_DIR} ftp
 
 # allow to upload thru FTP
 sed -i 's/#write_enable=/write_enable=/g' /etc/vsftpd.conf
+# Chroot to home folder
+sed -i '0,/#chroot_local_user=/{s/#chroot_local_user=/chroot_local_user=/}' /etc/vsftpd.conf
+echo "pasv_min_port=21100" >> /etc/vsftpd.conf
+echo "pasv_max_port=21110" >> /etc/vsftpd.conf
 
 # set Web server password from secret
 if [ ! -z ${PAPERLESS_WEBSERVER_PWD_FILE} -a -f ${PAPERLESS_WEBSERVER_PWD_FILE} ]; then
@@ -22,20 +26,23 @@ if [ ! -z ${PAPERLESS_PASSPHRASE_FILE} -a -f ${PAPERLESS_PASSPHRASE_FILE} ]; the
     export PAPERLESS_PASSPHRASE=`cat $PAPERLESS_PASSPHRASE_FILE`;
 fi
 
-ls -l /usr/src/paperless/data
+# set FTP user password from secret
+if [ ! -z ${PAPERLESS_FTP_PWD_FILE} -a -f ${PAPERLESS_FTP_PWD_FILE} ]; then
+    PAPERLESS_FTP_PWD=`cat $PAPERLESS_FTP_PWD_FILE`;
+fi
+
 
 # Migrate database
 echo Migrate database
 /usr/src/paperless/src/manage.py migrate
-
-ls -l /usr/src/paperless/data
 
 # Create webserver user
 echo Create webserver user
 # https://stackoverflow.com/a/42491469/1937418
 /usr/src/paperless/src/manage.py createsuperuser2 --username ${PAPERLESS_WEBSERVER_USER} --email paperless@test.com --password ${PAPERLESS_WEBSERVER_PWD} --noinput
 
-ls -l /usr/src/paperless/data
+# create FTP user
+useradd -d ${PAPERLESS_CONSUMPTION_DIR} -p `openssl passwd -1 ${PAPERLESS_FTP_PWD}` ${PAPERLESS_FTP_USER}
 
 # start FTP server
 echo start FTP server
